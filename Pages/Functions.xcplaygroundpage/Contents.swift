b6e48d32506314aee61a4cf8d9683000e43a9577
hailey Lee <foxwavez@gmail.com>
2018-08-06 21:41:17 +0900

    섹션 5 - 함수에서 값을 리턴하기 - 화씨온도를 섭씨온도 바꾸는 문제를 펑션을 이용해 풀어보았다. -> 온도는 소수점이라 Double을 써야 했었다.

--- Pages/Functions.xcplaygroundpage/Contents.swift
+++ Pages/Functions.xcplaygroundpage/Contents.swift
@@ -1,8 +1,25 @@
-
-func calculateArea(width: Int, height: Int) { /* 함수는 특정작업을 실행하는 코드 뭉치 */
+/* 함수는 특정작업을 실행하는 코드 뭉치 */
+func calculateArea(width: Int, height: Int) -> Int { /* 리턴 타입을( -> ) 지정해주자 마자 컴파일러가 함수의 반환값이 없다고 알려준다 버그를 사전에 예방해줄 수 있다 */
     let area = width * height
-    print(area)
+    return area
 }
 
-//W
-calculateArea(width: 17, height: 10)
+//Wall #1
+let areaOfWall = calculateArea(width: 17, height: 10)
+
+//Wall #2
+calculateArea(width: 14, height: 3)
+
+// 직접 문제 풀기 섭씨를 화씨로 바꿔주기
+func calculateTemperture(F: Int) -> Int {
+    let temperture = (F-32) * 5/9
+    return temperture
+}
+let tempertureOfC = calculateTemperture(F: 90)
+
+// 문제의 답
+func covertToCelsius(temperature: Double) -> Double {
+    let temperatureInCelsius = (temperature - 32) * (5/9)
+    return temperatureInCelsius
+}
+covertToCelsius(temperature: 80)
