f9465db2609cab93b24be5f606127d0c4909e9ea
hailey Lee <foxwavez@gmail.com>
2018-08-07 20:08:43 +0900

    섹션 6 - 프로퍼티 - Computed Property - 값이 저장되지 않고 매 번 새로운 값을 저장하는 프로퍼티 - Stored Propery  - 값이 저장되어 있는 프로퍼티: 변수나, 상수의 선언한 값을 가질 수 있다.

--- Pages/OOP.xcplaygroundpage/Contents.swift
+++ Pages/OOP.xcplaygroundpage/Contents.swift
@@ -15,13 +15,15 @@ resolutuon2.height = 30
 resolutuon2.width = 15
 
 class VideoMode {
-    var resolution = Resolutuon() /* 프로퍼티 */
+    var resolution = Resolutuon() /* 프로퍼티: 클래스, 구조체, 열겨형에 들어있는 값.
+     
+ */
     var frameRate = 0.0
     var name = ""
 }
 
 let videoMode = VideoMode()
-/*  인스턴스.       컨스턴트 */
+/*  let 컨스턴트=상수  인스턴스 */
 videoMode.frameRate
 videoMode.name
 videoMode.resolution.height
@@ -46,3 +48,46 @@ tenEigty.frameRate
 anotherTenEighty.frameRate
 
 /* 스트럭쳐는 보통 상대적으로 간단한 데이터 타입을 사용하고 싶을 때 사용한다.*/
+
+// Stored Propery  - 값이 저장되어 있는 프로퍼티: 밸류어블(변수)이나, 컨스턴트(상수) 선언한 값을 가질 수 있다.
+struct FixedLengthRange {
+    var firstValue: Int
+    let length: Int
+}
+
+var rangOfThreeItmes = FixedLengthRange(firstValue: 0, length: 3) /* 스트럭쳐는 초기화 함수를 자동으로 생성해준다. */
+rangOfThreeItmes.firstValue = 6
+
+let rangOfFourItems = FixedLengthRange(firstValue: 0, length: 4) /* rangOfFourItems가 컨스턴트(상수let) 이기 때문에 값을 바꿀 수가 없다 */
+// rangOfThreeItmes.firstValue = 6
+
+//Computed Property - 값이 저장되지 않고 매 번 새로운 값을 저장하는 프로퍼티
+struct Point {
+    var x = 0.0
+    var y = 0.0
+}
+struct Size {
+    var width = 0.0
+    var height = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get { /*get 안에 들어가는 코드는 center라는 프로퍼티를 호출할 때 실행된다 */
+            let centerX = origin.x + (size.width / 2)
+            let ceinerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: ceinerY)
+        }
+        set(newCenter) { /* 새트는 var center에 값을 할당해줄 때 */
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.x - (size.height / 2)
+        }
+    }
+}
+var square = Rect(origin:Point(x: 0.0, y: 0.0), size: Size(width: 10, height: 10))
+let initialCenter = square.center
+square.center = Point(x: 15.0, y: 15.0)
+square.origin
+
+//Type Property - 인스턴스에 귀속되어 있지 않고 타입자체에 연관되어 있는 프로퍼티
